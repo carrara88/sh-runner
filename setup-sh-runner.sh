8b4d989b4147c150307e6c678b7edd43463af531
CP <47906052+carrara88@users.noreply.github.com>
2022-04-10 00:05:35 +0200

    setup

--- setup-sh-runner.sh
+++ setup-sh-runner.sh
@@ -21,7 +21,7 @@ else
     sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
     sudo apt-get install nodejs -y
 fi
-    sudo cat << EOF > /lib/systemd/system/node_sh_runner.service
+    sudo cat << EOF > /etc/systemd/system/node_sh_runner.service
 [Unit]
 Description=sh-runner nodejs service
 After=network.target
