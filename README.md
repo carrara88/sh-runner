9d29940d4c7ab7de4777e1d8663dd357c5ee0c58
CP <47906052+carrara88@users.noreply.github.com>
2022-04-09 04:37:43 +0200

    setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # sh-runner
+
+Setup script (nginx + nodejs)
+```
+    $ curl https://raw.githubusercontent.com/carrara88/sh-runner/main/sh-runner-server/setup-runner.sh -o setup-runner.sh
+    $ sudo chmod 755 setup-runner.sh
+    $ ./setup-runner.sh
+```
--- sh-runner-server/setup-runner.sh
+++ sh-runner-server/setup-runner.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
 
-##
+################################################################
 # Server setup for 'sh-runner'
 # run this script with:
-# $ curl https://raw.githubusercontent.com/carrara88/sh-runner/sh-runner-server/setup-runner.sh -o setup-runner.sh
+# $ curl https://raw.githubusercontent.com/carrara88/sh-runner/main/sh-runner-server/setup-runner.sh -o setup-runner.sh
 # $ ./setup-runner.sh
-######
-echo "Setup (Node.js + nginx)"
+################################################################
+echo "################################################################-> sh-runner (Node.js + npm + nginx + @angular/cli)"
 
-echo "-> nodejs"
+echo "################################################################-> setup: nodejs"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo apt-get install nodejs -y
 
-echo "-> nginx"
+echo "################################################################-> setup: npm"
+sudo apt-get install npm -y
+
+echo "################################################################-> setup: nginx"
 sudo apt install nginx -y
 
-echo "Setup completed!"
+echo "################################################################-> setup: @angular/cli"
+npm install -g @angular/cli -y
 
-cd /var/www
-git clone https://github.com/carrara88/sh-runner.git /var/www/html/runner
+echo "################################################################-> clone: sh-runner"
+sudo rm -rvf /var/sh-runner
+git clone https://github.com/carrara88/sh-runner.git /var/sh-runner
 
+echo "################################################################-> build: sh-runner"
+ng build /var/sh-runner/sh-runner-app
+
+echo "################################################################-> Setup completed!"
 
