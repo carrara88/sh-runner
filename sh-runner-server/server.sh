58cbe415c1711eedab3751c0425066b405a52b39
CP <47906052+carrara88@users.noreply.github.com>
2022-04-10 22:21:59 +0200

    update server.sh

--- sh-runner-server/server.sh
+++ sh-runner-server/server.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# ASKS
-_ASKSERV(){ # 
-    read -p "${LINECAP} Certbot email:" ASKSERV
-}
-
 for x in /var/www/sh-installer/installers/*.installer.sh
 do
     echo -n "$x "
@@ -15,7 +10,6 @@ echo "Total:" $total
 
 case "$1" in
     "server_info")
-        _ASKSERV
         echo "{ 'status':'running', 'serv':'${ASKSERV}' }"
     ;;
     "server_status")
