84e19710f177e3e23b95f18bba7a31280931db6f
CP <47906052+carrara88@users.noreply.github.com>
2022-04-11 01:12:06 +0200

    update server.sh

--- sh-runner-server/server.sh
+++ sh-runner-server/server.sh
@@ -7,18 +7,21 @@ AVAILABLE_INSTALLERS=( ${INSTALLER_DIR}/*${INSTALLERS_EXTENSION} )
 AVAILABLE_INSTALLERS=( "${AVAILABLE_INSTALLERS[@]##*/}" )
 AVAILABLE_INSTALLERS=( "${AVAILABLE_INSTALLERS[@]%${INSTALLERS_EXTENSION}}" )
 
-printf -v LIST "\',\'%s" "${AVAILABLE_INSTALLERS[@]}"
-LIST=${LIST:3} 
+printf -v INSTALLERS_LIST "\',\'%s" "${AVAILABLE_INSTALLERS[@]}"
+INSTALLERS_LIST=${INSTALLERS_LIST:3} 
 
 HOSTNAME=$(eval "hostname -I")
 HOSTDATA=($HOSTNAME)
 HOST_IP=${HOSTDATA[0]}
 
-echo "HOSTNAME: ${HOST_IP}"
+
+SERVERINFO(){
+    echo "{ 'ip':'${HOST_IP}', 'installer_dir':'${INSTALLER_DIR}', 'installers_extension':'${INSTALLERS_EXTENSION}', 'installers':['${INSTALLERS_LIST}'] }"
+}
 
 case "$1" in
     "server_info")
-        echo "{ 'ip':'${HOST_IP}', 'installer_dir':'${INSTALLER_DIR}', 'installers_extension':'${INSTALLERS_EXTENSION}', 'installers':['${LIST}'] }"
+        SERVERINFO
     ;;
     "server_status")
         echo "{ 'auth':'cookies' } "
