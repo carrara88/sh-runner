f9fd87304c0011efc918442ea1244efb5ebd8c32
CP <47906052+carrara88@users.noreply.github.com>
2022-04-10 22:04:33 +0200

    update 8 files and delete 1 file

--- sh-runner-app/src/app/runner-setup/runner-setup.component.ts
+++ sh-runner-app/src/app/runner-setup/runner-setup.component.ts
@@ -47,8 +47,7 @@ export class RunnerSetupComponent implements OnInit {
     console.log('reactive form submit', this.form.value);
   }
   ngOnInit(): void {
-    console.log(self.location.host);
-    let server = this.setupService.get('http://localhost:3001/server_status');
+    let server = this.setupService.get('http://'+ self.location.host +':3001/server_status');
     server.subscribe(
       (data:any)=>{
         console.log('get server_status:', data);
