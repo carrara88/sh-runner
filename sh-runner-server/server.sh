675c6ad97856f9ca668d6de6eb5012bb3aa2468f
CP <47906052+carrara88@users.noreply.github.com>
2022-04-10 22:48:41 +0200

    update server.sh

--- sh-runner-server/server.sh
+++ sh-runner-server/server.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-for x in /var/www/sh-installer/installers/*.installer.sh
-do
-    echo -n "$x "
-done
+AVAILABLES in /var/www/sh-installer/installers/*.installer.sh
+
+LIST=$(IFS=, ; echo "${AVAILABLES[*]}")
 
 HOSTNAME=$(eval "hostname -I")
 HOSTDATA=($HOSTNAME)
 HOST_IP=$HOSTDATA[0]
+
 echo "HOSTNAME: ${HOST_IP}"
 
 case "$1" in
     "server_info")
-        echo "{ 'status':'running', 'serv':'${HOST_IP}' }"
+        echo "{ 'status':'running', 'ip':'${HOST_IP}', 'availables':[${LIST}] }"
     ;;
     "server_status")
         echo "{ 'auth':'cookies' } "
