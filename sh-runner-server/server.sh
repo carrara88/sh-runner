46e45bba3608ea910a10b1b010cf3624fac032a2
CP <47906052+carrara88@users.noreply.github.com>
2022-04-11 02:03:45 +0200

    update server.sh

--- sh-runner-server/server.sh
+++ sh-runner-server/server.sh
@@ -14,7 +14,7 @@ HOSTNAME=$(eval "hostname -I")
 HOSTDATA=($HOSTNAME)
 HOST_IP=${HOSTDATA[0]}
 
-a=($(exec systemctl --type=service | sed 's|.* ||'))
+a=($(exec systemctl --type=service | sed '\n ||'))
 printf -v al "\",\"%s" "${a[@]}"
 echo "${al}"
 SERVER_INFO(){
