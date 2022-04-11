135005dae1c1f2785035703f7c0972c6f164178b
CP <47906052+carrara88@users.noreply.github.com>
2022-04-11 05:41:17 +0200

    update update-sh-runner.sh

--- update-sh-runner.sh
+++ update-sh-runner.sh
@@ -6,6 +6,6 @@
 sudo systemctl stop node_sh_runner
 sudo systemctl start node_sh_runner
 sudo rm -vf /var/www/sh-runner/sh-runner-server/server.sh
-sudo touch -v /var/www/sh-runner/sh-runner-server/server.sh
+sudo touch /var/www/sh-runner/sh-runner-server/server.sh
 sudo bash -c "curl https://raw.githubusercontent.com/carrara88/sh-runner/dev/sh-runner-server/server.sh > /var/www/sh-runner/sh-runner-server/server.sh"
 sudo chmod 755 /var/www/sh-runner/sh-runner-server/server.sh
