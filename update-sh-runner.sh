4395974485c2f7f8e1d7119b103433f55891df54
CP <47906052+carrara88@users.noreply.github.com>
2022-04-11 05:20:09 +0200

    update update-sh-runner.sh

--- update-sh-runner.sh
+++ update-sh-runner.sh
@@ -4,5 +4,5 @@
 # sudo chmod 755 update-sh-runner.sh
 
 sudo rm -f /var/www/sh-runner/sh-runner-server/server.sh
-sudo curl https://raw.githubusercontent.com/carrara88/sh-runner/dev/sh-runner-server/server.sh -O /var/www/sh-runner/sh-runner-server/server.sh
+sudo curl https://raw.githubusercontent.com/carrara88/sh-runner/dev/sh-runner-server/server.sh > /var/www/sh-runner/sh-runner-server/server.sh
 sudo chmod 755 /var/www/sh-runner/sh-runner-server/server.sh
