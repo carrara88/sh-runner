07e1814fe58ca78e9405bb0f016bfc4f3a905938
CP <47906052+carrara88@users.noreply.github.com>
2022-04-11 03:32:41 +0200

    update server.sh

--- sh-runner-server/server.sh
+++ sh-runner-server/server.sh
@@ -7,6 +7,10 @@ AVAILABLE_INSTALLERS=( ${INSTALLER_DIR}/*${INSTALLERS_EXTENSION} )
 AVAILABLE_INSTALLERS=( "${AVAILABLE_INSTALLERS[@]##*/}" )
 AVAILABLE_INSTALLERS=( "${AVAILABLE_INSTALLERS[@]%${INSTALLERS_EXTENSION}}" )
 
+
+USERS=($(eval getent passwd {$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)..$(awk '/^UID_MAX/ {print $2}' /etc/login.defs)} | cut -d: -f1 ))
+
+
 printf -v INSTALLERS_LIST "\",\"%s" "${AVAILABLE_INSTALLERS[@]}"
 INSTALLERS_LIST=${INSTALLERS_LIST:3} 
 
@@ -25,7 +29,7 @@ EXITED_SERVICES_LIST=${EXITED_SERVICES_LIST:3}
 
 SERVER_INFO(){
 cat << EOF | sudo tee -a /var/www/html/runner/sh-runner/server_info.json
-{ "ip":"${HOST_IP}", "installer_dir":"${INSTALLER_DIR}", "installers_extension":"${INSTALLERS_EXTENSION}", "installers":["${INSTALLERS_LIST}"] }
+{ "ip":"${HOST_IP}", "users":"${USERS}","installer_dir":"${INSTALLER_DIR}", "installers_extension":"${INSTALLERS_EXTENSION}", "installers":["${INSTALLERS_LIST}"] }
 EOF
 }
 
