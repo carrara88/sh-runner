1a59b98c01fcec4395afda8f9e094e0accca4939
CP <47906052+carrara88@users.noreply.github.com>
2022-04-12 02:41:33 +0200

    update 10 files and delete 5 files

--- sh-runner-app/src/app/runner-auth/runner-auth.component.ts
+++ sh-runner-app/src/app/runner-auth/runner-auth.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { SetupService } from '../services/setup.service';
 
 @Component({
   selector: 'app-runner-auth',
@@ -10,15 +11,27 @@ export class RunnerAuthComponent implements OnInit {
 
   openSignIn=true;
   form: FormGroup;
-  constructor(private formBuilder: FormBuilder) {
+
+  constructor(private formBuilder: FormBuilder, public setupService: SetupService) {
     this.form = this.formBuilder.group({
-      host: [self.location.host+':3001', Validators.required],
+      host: [self.location.host, Validators.required],
+      port: ['3001', Validators.required],
       username: ['admin', Validators.required],
-      password: ['instance-', Validators.required],
+      password: ['', Validators.required],
     });
   }
 
+  authenticate(){
+    let server_auth = this.setupService.get('http://'+ self.location.host +':3001/server_signin/'+this.form.controls["username"].value+"/"+this.form.controls["password"].value);
+    server_auth.subscribe(
+      (data:any)=>{
+        console.log('get authenticate:', data);
+      }
+    );
+  }
+
   ngOnInit(): void {
+
   }
 
 }
