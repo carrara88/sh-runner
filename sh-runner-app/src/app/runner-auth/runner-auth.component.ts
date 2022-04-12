25ba478734624789a1ca6d638dc4b0275d37c28c
CP <47906052+carrara88@users.noreply.github.com>
2022-04-12 02:28:31 +0200

    update runner-auth.component.html and runner-auth.component.ts

--- sh-runner-app/src/app/runner-auth/runner-auth.component.ts
+++ sh-runner-app/src/app/runner-auth/runner-auth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-runner-auth',
@@ -8,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class RunnerAuthComponent implements OnInit {
 
   openSignIn=true;
-  constructor() { }
+  form: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      host: [self.location.host+':3001', Validators.required],
+      username: ['admin', Validators.required],
+      password: ['instance-', Validators.required],
+    });
+  }
 
   ngOnInit(): void {
   }
